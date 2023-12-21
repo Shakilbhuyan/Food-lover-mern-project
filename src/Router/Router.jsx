bd134ce9ea8c8f15203e1cf06b74eeda8f3111f3
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-12-21 19:27:53 +0600

    shared page done

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/home/Home";
+import MainLayouts from "../layouts/MainLayouts";
 
 const router = createBrowserRouter([
     {
       path: "/",
+      element:<MainLayouts></MainLayouts>,
       children: [
         {
           path:'/',
--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navber from '../pages/shared/navigation/Navber';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shared/footer/Footer';
 
 const MainLayouts = () => {
     return (
         <div>
             <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
